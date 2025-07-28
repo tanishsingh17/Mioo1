2a5e3f7df065482ca28020c52b8fa43fafe7cc3d
Tanish Singh Thakur <144647913+tanishsingh17@users.noreply.github.com>
2025-07-28 13:22:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelector('.icon.twitter')?.addEventListener('click', () => {
+  window.open('https://x.com/MiooHL', '_blank');
+});
+
+document.querySelector('.icon.discord')?.addEventListener('click', () => {
+  window.open('https://discord.gg/xPucAXZ6', '_blank');
+});
+
+document.querySelector('.icon.checker')?.addEventListener('click', () => {
+  window.open('https://tanishsingh17.github.io/final-wallet/', '_blank');
+});
